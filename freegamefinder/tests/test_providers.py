17ded694d25d6d049d0e7694efb0dcc38c2dad4d
ababyduck <kassisaf@gmail.com>
2020-08-22 15:45:32 -0700

    Test for correct number of posts

--- freegamefinder/tests/test_providers.py
+++ freegamefinder/tests/test_providers.py
@@ -1,4 +1,5 @@
 import unittest as ut
+from random import randint
 from praw.models import ListingGenerator
 from praw.models import Submission
 from freegamefinder.providers.subreddits import gamedeals as rgd
@@ -7,13 +8,18 @@ from freegamefinder.providers.subreddits import freegamefindings as rfgf
 
 class TestSubredditProviders(ut.TestCase):
     def test_get_posts(self):
-        """ Tests that Subreddit objects can be created and that praw is able to return valid posts """
-        expected_length = 3
-        r_gamedeals = rgd.GameDeals(limit=expected_length)
-        r_freegamefindings = rfgf.FreeGameFindings(limit=expected_length)
+        """ Tests that Subreddit objects can be created and that
+        praw retrieves the expected number of valid posts
+        """
+        r_gamedeals = rgd.GameDeals()
+        r_freegamefindings = rfgf.FreeGameFindings()
         for sub in [r_gamedeals, r_freegamefindings]:
+            sub.limit = randint(1, 3)
+            counter = 0
             posts = sub.get_posts()
             self.assertIsInstance(posts, ListingGenerator)
-            for i, post in enumerate(posts):
+            for post in posts:
                 self.assertIsInstance(post, Submission)
                 self.assertIsNotNone(post.title)
+                counter += 1
+            self.assertEqual(counter, sub.limit)
